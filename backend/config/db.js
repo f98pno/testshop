9e2f93a6b3e3e9f8094f73b060f7215d9c1931e6
Peter <peter.novak.m@gmail.com>
2021-06-04 19:44:48 +0800

    Adding eslint to backend and colors package

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,18 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifinedTopology: true,
+      useUnifiedTopology: true,
       useNewUrlParser: true,
-      useCreateIndex: true
-    })
+      useCreateIndex: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    
-
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
   }
+};
 
-}
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,32 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import products from './data/products.js'
+import express from 'express';
+import dotenv from 'dotenv';
 
-dotenv.config()
+/* eslint-disable no-unused-vars */
+import colors from 'colors';
+/* eslint-enable no-unused-vars */
 
-const app = express()
+import connectDB from './config/db.js';
+import products from './data/products.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+  res.send('API is running...');
+});
 
 app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+  res.json(products);
+});
 
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${5000}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${5000}`.blue));
