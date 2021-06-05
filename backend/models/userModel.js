13affb86892ec957f54e9f8db9c18341dbb48a7c
Peter <peter.novak.m@gmail.com>
2021-06-05 17:52:57 +0800

    Setting up product routes

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    requied: true,
+  },
+  email: {
+    type: String,
+    requied: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    requied: true,
+  },
+  isAdmin: {
+    type: String,
+    requied: true,
+    default: false,
+  },
+}, {
+  timestapns: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
