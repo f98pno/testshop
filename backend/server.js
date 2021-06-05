ac228ec4a750aa3012cde4a664b77ce1998f04f2
Peter <peter.novak.m@gmail.com>
2021-06-05 19:00:35 +0800

    Adding error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
 /* eslint-disable no-unused-vars */
 import colors from 'colors';
-/* eslint-enable no-unused-vars */
-
+/* eslint-disable no-unused-vars */
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 
@@ -15,11 +14,19 @@ connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('API is running...');
+  res.send('API is running....');
 });
 
 app.use('/api/products', productRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${5000}`.blue));
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold,
+  ),
+);
