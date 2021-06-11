11b9897a4942cbc6d14c87ceebf63b0a5c5f1d43
Peter <peter.novak.m@gmail.com>
2021-06-11 23:47:43 +0800

    Product controller and delete semicolons

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,28 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    requied: true,
-  },
-  email: {
-    type: String,
-    requied: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    requied: true,
-  },
-  isAdmin: {
-    type: String,
-    requied: true,
-    default: false,
-  },
-}, {
-  timestapns: true,
-});
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            requied: true,
+        },
+        email: {
+            type: String,
+            requied: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            requied: true,
+        },
+        isAdmin: {
+            type: String,
+            requied: true,
+            default: false,
+        },
+    },
+    {
+        timestapns: true,
+    }
+)
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema)
 
-export default User;
+export default User
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,33 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from 'express'
+import dotenv from 'dotenv'
 /* eslint-disable no-unused-vars */
-import colors from 'colors';
+import colors from 'colors'
 /* eslint-disable no-unused-vars */
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
 
-dotenv.config();
+dotenv.config()
 
-connectDB();
+connectDB()
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send('API is running....');
-});
+    res.send('API is running....')
+})
 
-app.use('/api/products', productRoutes);
+app.use('/api/products', productRoutes)
 
-app.use(notFound);
-app.use(errorHandler);
+app.use(notFound)
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold,
-  ),
-);
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+            .bold
+    )
+)
